fa9949352c43f771231736205a683da2631e3a3f
nachtjasmin <nachtjasmin@outlook.de>
2021-04-14 15:36:37 +0200

    add repro

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Tailwind from "./Tailwind.svelte";
+</script>
+
+<Tailwind />
+
+<div class="w-64 h-64 bg-blue-300">
+</div>
---
+++ src/Tailwind.svelte
@@ -0,0 +1,6 @@
+<style global style lang="postcss">
+	/* stylelint-disable */
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
